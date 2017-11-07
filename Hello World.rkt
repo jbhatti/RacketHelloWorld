be2760bc2905cd6b93a090a91c764f4d15dc99b7
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-11-06 22:08:45 -0500

    import images

--- Hello World.rkt
+++ Hello World.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Hello World|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Hello World|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 (+ 1 1)
 (string-append "hello " "world")
 (and (or
@@ -10,3 +10,4 @@
       )
       (string=? "hello world" "good morning"))
      (>= (+ (string-length "hello world") 60) 80))
+(circle 10 "solid" "red")
